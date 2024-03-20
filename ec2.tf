b35b3b6a7f4ff17edd62bff727b3fcd1ff1cfbfa
HenryMichel237 <tessahhenrie.michel@ictuniversity.org>
2024-03-20 03:08:40 +0100

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "server1" {
-    ami = ""
+    ami = "ami-02d7fd1c2af6eead0"
     instance_type = "t2.micro"
     vpc_security_group_ids = [ aws_security_group.sg1.id ]
     availability_zone = "us-east-1a"
     subnet_id = aws_subnet.private1.id
-    user_data = file(code.sh)
+    user_data = file("code.sh")
     tags = {
       Name = "webserner1"
     }
@@ -16,7 +16,7 @@ resource "aws_instance" "server2" {
   vpc_security_group_ids = [ aws_security_group.sg1.id ]
   availability_zone = "us-east-1b"
   subnet_id = aws_subnet.private2.id
-  user_data = file(code.sh)
+  user_data = file("code.sh")
   tags={
     Name = "webserver-2"
   }
